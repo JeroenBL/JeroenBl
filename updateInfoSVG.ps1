f90ed6538f25251223d825ac45a73512a248ad24
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2024-02-08 13:10:01 +0100

    wip

--- updateInfoSVG.ps1
+++ updateInfoSVG.ps1
@@ -5,8 +5,6 @@ if (($today -eq 'Saturday') -or ($today -eq 'Sunday')){
 }
 $currentDay = $today.ToString().ToLower()
 
-$publicRepos = (irm https://api.github.com/orgs/Tools4everBV).public_repos
-
 $svgContent = $svgContent.replace("{day}", $currentDay)
 $svgContent = $svgContent.replace("{000}", $publicRepos)
 $svgContent | Set-Content ./infoUpdated.svg -Encoding utf8 -Force
